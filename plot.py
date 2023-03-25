73d2b868059b3b7559fde7d65c3bc761b003b74b
Ryn Oliphant <rynoliphant@u.boisestate.edu>
2023-03-25 11:44:52 -0600

    Code now creates nice plots

--- plot.py
+++ plot.py
@@ -3,18 +3,18 @@ import numpy as np
 import sys
 
 filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py ARG1 ARG2 
-data = np.loadtxt(fname=filename, delimiter=' ', comments='"', usecols=range(6))   # Attempts to load filename into local variable data.
+data = np.loadtxt(fname=filename,delimiter=' ',comments='"',usecols=range(6))   # Attempts to load filename into local variable data.
 
-## Part 0
+## Part 0 (Done)
 # Figure out what arguments to add to the loadtxt function call
 # so that numbers are loaded into the local function 'data'.
 # Hint: look for arguments like 'skiprows' and 'delimiter'
 # Check by running:
-#   $ python plot.py raw-data/Sp22_245L_sec-001_tensiletest-pekk_bulk.raw
+#   $ python plot.py raw_data/Sp22_245L_sec-001_tensiletest-pekk_bulk.raw
 # at the command line.
 
 
-## Part 1
+## Part 1 (Done)
 # Figure out what columns and rows of data we need to plot
 # Stress (y-axis) vs Strain (x-axis)
 # plot raw-data/Sp22_245L_sec-001_tensiletest-pekk_bulk.raw
@@ -22,13 +22,13 @@ data = np.loadtxt(fname=filename, delimiter=' ', comments='"', usecols=range(6))
 # plt.plot(xdata, ydata, arguments-to-make-plot-pretty)
 
 
-## Part 2
+## Part 2 (Done)
 # Check to see if your code in part 1 will plot all of the files in raw-data/
 # Edit the files (use git liberally here!) to make them more usable
 # Don't worry about deleting parts you might need later -- that's why we use git!
 
 
-## Part 3
+## Part 3 (Done)
 # Use linear regression to calculate the slope of the linear part of
 # the stress-strain data. Plot your line against the data to make 
 # sure it makes sense! Use the slope of this line to calculate and print
